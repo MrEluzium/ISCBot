783464348e36f5ccce4b4d6cc75b22b88bad6d04
Artem Eluzium <mreluzium@mail.ru>
2021-09-07 21:47:15 +0500

    Licensed under GNU AGPLv3

--- db_analytics.py
+++ db_analytics.py
@@ -1,3 +1,20 @@
+# ISCBot. Telegram school helper for students.
+# Copyright (C) 2021 Artem Vorochaev (vorochaev2004@live.ru, github.com/Hymiside)
+#                    Artem Voronov (mreluzium@mail.ru, github.com/MrEluzium)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sqlite3
 from typing import Tuple
 
